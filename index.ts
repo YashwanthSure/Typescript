5a7e36a47db6eccc5fd25ecf2fa96f0eed9fa2b3
Yashwanth sure <yaswanthsure2004@gmail.com>
2025-03-30 13:05:49 +0530

    updtaed changes

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 function sum(a :number,b: number) {
     return a + b;
 }
-let sumResult = sum(6, 10);
+let sumResult = sum(13, 10);
 console.log(sumResult); // Output: 15
+function greet(name:string):string{
+    return name + "Yashwanth Sure"
+}
+greet("Hello")
