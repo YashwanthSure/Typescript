3e8fdd4174ea47dbe6cb38427e1947892d7ff593
Yashwanth sure <yaswanthsure2004@gmail.com>
2025-03-30 12:41:56 +0530

    changed

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+function sum(a, b) {
+    return a + b;
+}
+let sumResult = sum(5, 10);
+console.log(sumResult); // Output: 15
---
+++ index.ts
@@ -0,0 +1,5 @@
+function sum(a :number,b: number) {
+    return a + b;
+}
+let sumResult = sum(5, 10);
+console.log(sumResult); // Output: 15
